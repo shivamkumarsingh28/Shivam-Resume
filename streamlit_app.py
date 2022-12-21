ae1cf7580a1efd5669c73865da9c0034c622985c
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2022-12-21 16:34:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,12 @@ import streamlit as st
 from PIL import Image
 import smtplib
 import requests
-import hydralit_components as hc
-from streamlit_player import st_player, _SUPPORTED_EVENTS
 import datetime
 import base64
+from streamlit_player import st_player
 import streamlit.components.v1 as components
-from gsheetsdb import connect
 
-# Define your javascript
 
-#make it look nice from the start
-# st.set_page_config(layout='wide',initial_sidebar_state='collapsed')
 st.set_page_config(
      page_title="Shivam Resume",
      page_icon="🧊",
@@ -32,15 +27,8 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 #####################
 
-conn= connect()
 
-def SaeeAM_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
 
-sheet_url = "https://docs.google.com/spreadsheets/d/1AUUOpmJuftHrZ3RCjnKHGGZ1mF9GdLQtx3ravbNDYyM/edit?usp=sharing"
-Database = SaeeAM_query(f'SELECT * FROM "{sheet_url}"')
 
 def datafetch(a, b, c):
   col1, col2 = st.columns([4,1])
@@ -87,27 +75,6 @@ def txt5(a, b, c):
 #####################
 
 
-# specify the primary menu definition
-menu_data = [
-    {'id':'Achieve','icon': "fas fa-archive", 'label':"Achievement"},
-    {'id':'Projects','icon':"fas fa-project-diagram",'label':"Projects"},
-    {'id':'Past','icon': "fas fa-angle-left", 'label':"Was Do"},#no tooltip message
-    {'id':'Present','icon': "fas fa-stop-circle", 'label':"Doing"},
-    {'id':'Future','icon': "fas fa-angle-right", 'label':"Will Do"},
-]
-
-over_theme = {'txc_inactive': '#FFFFFF'}
-menu_id = hc.nav_bar(
-    menu_definition=menu_data,
-    override_theme=over_theme,
-    home_name='Shivam Kumar Singh',
-    login_name='Social Media',
-    hide_streamlit_markers=True, #will show the st hamburger as well as the navbar now!
-    sticky_nav=True, #at the top or not
-    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
-)
-
-
 with st.sidebar:
   with st.form("my_form", clear_on_submit=True):
             name= st.text_input(label="Name",value="", placeholder="Enter Name Here", type="default", key="name")
@@ -128,22 +95,21 @@ with st.sidebar:
                 st.success('Done')
                 st.balloons()
 
-with open("style.css") as f:
-    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+# with open("style.css") as f:
+#     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 #####################
-if menu_id == "Shivam Kumar Singh":
-  
-  st.write('''
+
+st.write('''
   # SHIVAM KUMAR SINGH, Entrepreneur.
   ##### *Resume* 
   ''')
   
-  image = Image.open('dp1.jpg')
-  st.image(image, width=150)
+image = Image.open('dp1.jpg')
+st.image(image, width=150)
   
-  st.markdown('## Summary', unsafe_allow_html=True)
-  st.info('''
+st.markdown('## Summary', unsafe_allow_html=True)
+st.info('''
   - **Who I AM** `Teacher, Entrepreneur, Sportperson, CEO, Leader, Programmer, Developer, Student, Humble Person, And Honest On Work`.
   - Experienced Educator, Entrepreneur, Youtuber, Mentor, Team Leader, Project Manager and Administrator with almost 5 years of experience in all domain i mention and a passion for delivering best through learning. 
   - Strong verbal and written communication skills as demonstrated by extensive participation as speaker .
@@ -151,113 +117,103 @@ if menu_id == "Shivam Kumar Singh":
   - My hobies `Painting, Writing, Dancing, Reading, Hockey, Running, Kabaddi, Cycling, Gym` that i like to do when i free and when i enjoy my life and when i disturb i do .
   ''')
 
-  st.markdown('''
+st.markdown('''
   ## Education
   ''')
 
 
-# Print results.
-  for edu in Database:
-    
-    if edu[13] != None:
-      # disc=edu[15].split(',',1)
-      datafetch(edu[13],edu[14], edu[15])
-  txt('**Advance Diploma In Computer** (IBM, Aictc, Nsti, DGT), *Nsti-Bangluru*, Bangluru',
+txt('**Advance Diploma In Computer** (IBM, Aictc, Nsti, DGT), *Nsti-Bangluru*, Bangluru',
   '2022-2024')
-  st.markdown('''
+st.markdown('''
   - PERCENTAGE: `Puruing`
   - SUBJECTS `Jave, Cloud Computing, Web Development, IT & Iot, And More...`.
   ''')
   
-  txt('**Programming, Data Structure And Algorithm With Python** (Aictc, Swayam, IIT-Madras), *Online*, Delhi',
+txt('**Programming, Data Structure And Algorithm With Python** (Aictc, Swayam, IIT-Madras), *Online*, Delhi',
   'OnGoing')
-  st.markdown('''
+st.markdown('''
   - PERCENTAGE: `Puruing`
   - SUBJECTS `Python, Python OOPS, Data Structure, Algorithms, And More...`.
   - Assignment Marks - Assignment - 1st-`60%`, 2nd- `70%`, 3rd-`70%`, 4th- `100%`, And Upcoming
   - Exam Date - 25-SEP, 2022
   ''')
   
-  txt('**Google Android Developer- Internship** (Aictc, Google, Intership, ), *Online*, Delhi',
+txt('**Google Android Developer- Internship** (Aictc, Google, Intership, ), *Online*, Delhi',
   'OnGoing')
-  st.markdown('''
-  - PERCENTAGE: `Puruing`
-  - SUBJECTS `Kotlin, Android Studio, XML, And More...`.
-  ''')
-  
-  txt('**Salesforce CRM- Internship** (Aictc, Salesforce, Intership, ), *Online*, Delhi',
-  'OnGoing')
-  st.markdown('''
-  - PERCENTAGE: `Puruing`
-  - SUBJECTS `Salesforce, Admin, Developer, And More...`.
-  ''')
+st.markdown('''
+- PERCENTAGE: `Puruing`
+- SUBJECTS `Kotlin, Android Studio, XML, And More...`.
+''')
 
-  txt('**Bachelors of Science** (BIOTECHNOLOGY), *JAMIA MILLIA ISLAMIA - STUDY CENTER*, DELHI',
-  '2019-2022')
-  st.markdown('''
-  - GPA: `PURSUING`
-  - Graduated with First Class Honors.
-  - SUBJECTS `MATH, BOTANY, ZOOLOGY`.
-  ''')
-  
-  txt('**ITI- TECHNICAL TRAINING** (WELDER TRADE), *AKS- ARAB KI SARAI*, DELHI-NIZAMUDDIN',
-  '2020-2021')
-  st.markdown('''
-  - PERCENTAGE: `75%`
-  - SUBJECTS `MATH & SCIENCE, TRADE THEORY, WORKSHOP-PRACTICAL, IT & COMMUNICATION SKILL, ENGINEERING DRAWING`.
-  ''')
-  
-  txt('**INTERMEDIATE 12TH** (SCIENCE - PCM), *SHYAM DARI YAMUNA HIGH SCHOOL*, BIHAR, *BIHAR BOARD*',
-  '2017-2019')
-  st.markdown('''
-  - PERCENTAGE: `62%`
-  - INTERMEDIATE WITH First Class Honors.
-  - SUBJECTS `PHYSICS, CHEMISTRY, MATH`.
-  ''')
-  
-  txt('**MATRICULATION 10TH** (ALL SUBJECT), *SARVODAYA BAL VIDYALAYA NO-1*, DELHI, *CBSE-BOARD*',
-  '2016-2017')
-  st.markdown('''
-  - GPA: `7`
-  - ALL SUBJECT- `MATH, SCIENCE, ENGLISH, SANSKRIT, SOCIAL-SCIENCE`.
-  ''')
+txt('**Salesforce CRM- Internship** (Aictc, Salesforce, Intership, ), *Online*, Delhi',
+'OnGoing')
+st.markdown('''
+- PERCENTAGE: `Puruing`
+- SUBJECTS `Salesforce, Admin, Developer, And More...`.
+''')
 
+txt('**Bachelors of Science** (BIOTECHNOLOGY), *JAMIA MILLIA ISLAMIA - STUDY CENTER*, DELHI',
+'2019-2022')
+st.markdown('''
+- GPA: `PURSUING`
+- Graduated with First Class Honors.
+- SUBJECTS `MATH, BOTANY, ZOOLOGY`.
+''')
 
-  #####################
-  st.markdown('''
-  ## Work Experience
-  ''')
-  for work in Database:
-    
-    if work[16] != None:
-      # disc=edu[15].split(',',1)
-      txt(work[16],work[17], work[18])
-  txt('**Content Creator**, [SaeeAM Education YouTube Channel](https://www.youtube.com/watch?v=PIKz3R4Nqpc&list=PLF7OcypGGslKsmS8uqCOjLoWJqYPai_Us)',
-  '2019-Present')
-  st.markdown('''
-  - Created `261-Videos` `Math, Science, English, Gk&GS`.
-  - Level `6th to 12th, ITI-Diploma, Graduation, Compedition Exam` `Math, Science, English, Gk&GS, Reasoning`.
-  ''')
+txt('**ITI- TECHNICAL TRAINING** (WELDER TRADE), *AKS- ARAB KI SARAI*, DELHI-NIZAMUDDIN',
+'2020-2021')
+st.markdown('''
+- PERCENTAGE: `75%`
+- SUBJECTS `MATH & SCIENCE, TRADE THEORY, WORKSHOP-PRACTICAL, IT & COMMUNICATION SKILL, ENGINEERING DRAWING`.
+''')
 
-  txt('**Content Creator**, [SaeeAM Coding YouTube Channel](https://www.youtube.com/channel/UCUSh5Tyq2G7kBrTTE005prA/playlists)',
-  '2019-Present')
-  st.markdown('''
-  - Created `100-Video` `Python, Streamlit, Django, React, Javascript, Linux, Google Cloud Computing, AWS-Azure, HTML, Css, SQL, Panda, Numpy, Matplot, Heroku, And More ...`.
-  ''')
-
-  txt('**Content Writer**, [SaeeAM Blog](https://g.page/r/CR93AGLEpeP_EAE) on Google My Business',
-  '2019-Present')
-  st.markdown('''
-  - Written `Many Blog` Education, Career, Job Fair, Services, And lOts of more...
-  ''')
-  
-  txt('**Private Institution Teacher**, Faculty of Science(PCM) , Pie Institute, Bihar',
+txt('**INTERMEDIATE 12TH** (SCIENCE - PCM), *SHYAM DARI YAMUNA HIGH SCHOOL*, BIHAR, *BIHAR BOARD*',
 '2017-2019')
-  txt('**Online Teacher**, SaeeAM Classes, Faculty Of Education, All Over India',
+st.markdown('''
+- PERCENTAGE: `62%`
+- INTERMEDIATE WITH First Class Honors.
+- SUBJECTS `PHYSICS, CHEMISTRY, MATH`.
+''')
+
+txt('**MATRICULATION 10TH** (ALL SUBJECT), *SARVODAYA BAL VIDYALAYA NO-1*, DELHI, *CBSE-BOARD*',
+'2016-2017')
+st.markdown('''
+- GPA: `7`
+- ALL SUBJECT- `MATH, SCIENCE, ENGLISH, SANSKRIT, SOCIAL-SCIENCE`.
+''')
+
+
+#####################
+st.markdown('''
+## Work Experience
+''')
+
+txt('**Content Creator**, [SaeeAM Education YouTube Channel](https://www.youtube.com/watch?v=PIKz3R4Nqpc&list=PLF7OcypGGslKsmS8uqCOjLoWJqYPai_Us)',
+'2019-Present')
+st.markdown('''
+- Created `261-Videos` `Math, Science, English, Gk&GS`.
+- Level `6th to 12th, ITI-Diploma, Graduation, Compedition Exam` `Math, Science, English, Gk&GS, Reasoning`.
+''')
+
+txt('**Content Creator**, [SaeeAM Coding YouTube Channel](https://www.youtube.com/channel/UCUSh5Tyq2G7kBrTTE005prA/playlists)',
+'2019-Present')
+st.markdown('''
+- Created `100-Video` `Python, Streamlit, Django, React, Javascript, Linux, Google Cloud Computing, AWS-Azure, HTML, Css, SQL, Panda, Numpy, Matplot, Heroku, And More ...`.
+''')
+
+txt('**Content Writer**, [SaeeAM Blog](https://g.page/r/CR93AGLEpeP_EAE) on Google My Business',
+'2019-Present')
+st.markdown('''
+- Written `Many Blog` Education, Career, Job Fair, Services, And lOts of more...
+''')
+
+txt('**Private Institution Teacher**, Faculty of Science(PCM) , Pie Institute, Bihar',
+'2017-2019')
+txt('**Online Teacher**, SaeeAM Classes, Faculty Of Education, All Over India',
 '2020-Present')
-  txt('**Career Guide**, Faculty Of Career, SaeeAM, All Over India',
+txt('**Career Guide**, Faculty Of Career, SaeeAM, All Over India',
 '2017-Present')
-  st.markdown('''
+st.markdown('''
 - Provided mentorship and supervision to junior students, school level, 10th./12th./B.Sc. students. 
 - Online/Offline Medium To Teach `Zoom, Meet`, `School/Institution/Collage`. 
 - Video Notes, Class Notes, Study Notes Prepare For Online/Offline Classes.
@@ -265,110 +221,78 @@ if menu_id == "Shivam Kumar Singh":
 - Lecture Note Public On Telegram Channels [SaeeAM Classes](https://t.me/+4Wwajk1RorA0OTE9)
 - Doubt Session Discuss Through Whats'app Group [SaeeAM Class](https://chat.whatsapp.com/KvqQTGzvYp405hofDsHyuO)
 ''')
-  
-  txt('**VODAFONE PVT.LTD**, SALES-TL, DELHI',
-  '2017')
-  st.markdown('''
-  - CTC-IN HAND: `240K Yearly`
-  - WHAT WE DO `Sales, Pitching, Product-Describe, And More ...`
-  - WHAT WE LEARN `Sales, Communication-skill, Personality-Development, Customer-handling, Product-Diling And More ...`.
-  ''')
 
-  txt('**Film Shooting PVT.LTD**, ALL TYPES WORK, ALL OVER INDIA',
-  '2018-2020')
-  st.markdown('''
-  - CTC-IN HAND: `360K Yearly`
-  - WHAT WE DO `Sales, Pitching, Product-Describe, And More ...`
-  - WHAT WE LEARN `Sales, Communication-skill, Personality-Development, Customer-handling, Product-Diling And More ...`.
-  ''')
-  txt('**YAMAHA PVT.LTD**, ITI-WELDER, UP',
-  '2022')
-  st.markdown('''
-  - CTC-IN HAND: `120K Yearly`
-  - WHAT WE DO `Engine Mounting, Gauging, Panasonic Robot Operator, Welding Frame, Quality Checking, And More ...`
-  - WHAT WE LEARN `Technical Knowledage, Apprentienship`.
-  ''')
+txt('**VODAFONE PVT.LTD**, SALES-TL, DELHI',
+'2017')
+st.markdown('''
+- CTC-IN HAND: `240K Yearly`
+- WHAT WE DO `Sales, Pitching, Product-Describe, And More ...`
+- WHAT WE LEARN `Sales, Communication-skill, Personality-Development, Customer-handling, Product-Diling And More ...`.
+''')
 
-  txt('**MARUTI SUZUKI PVT.LTD**, ITI-WELDER, DELHI',
-  '2022')
-  st.markdown('''
-  - CTC-IN HAND: `240K Yearly`.
-  ''')
+txt('**Film Shooting PVT.LTD**, ALL TYPES WORK, ALL OVER INDIA',
+'2018-2020')
+st.markdown('''
+- CTC-IN HAND: `360K Yearly`
+- WHAT WE DO `Sales, Pitching, Product-Describe, And More ...`
+- WHAT WE LEARN `Sales, Communication-skill, Personality-Development, Customer-handling, Product-Diling And More ...`.
+''')
+txt('**YAMAHA PVT.LTD**, ITI-WELDER, UP',
+'2022')
+st.markdown('''
+- CTC-IN HAND: `120K Yearly`
+- WHAT WE DO `Engine Mounting, Gauging, Panasonic Robot Operator, Welding Frame, Quality Checking, And More ...`
+- WHAT WE LEARN `Technical Knowledage, Apprentienship`.
+''')
 
-  
-  
-  #####################
+txt('**MARUTI SUZUKI PVT.LTD**, ITI-WELDER, DELHI',
+'2022')
+st.markdown('''
+- CTC-IN HAND: `240K Yearly`.
+''')
 
-  st.markdown('''
-  ## Skills
-  ''')
-  for skill in Database:
-    
-    if skill[19] != None:
-      # disc=edu[15].split(',',1)
-      txt3(skill[19],skill[20])
-  # 
-  st.markdown('''
-  ## Profile Links
-  ''')
-  for skill in Database:
-    
-    if skill[21] != None:
-      # disc=edu[15].split(',',1)
-      txt3(skill[21],skill[22])
+
 
 #####################
-if menu_id == "Social Media":
-  st.markdown('''
-  ## Social Media
-  ''')
-  for link in Database:
-    
-    if link[23] != None:
-      # disc=edu[15].split(',',1)
-      txt3(link[23],link[24])
-      st.markdown("***")
-  
 
-if menu_id == "Projects":
-  for projects in Database:
-    if projects[3] != None:
-      txt5(projects[3],projects[4], projects[5])
-      st.markdown("***")
-  
-  
+st.markdown('''
+## Skills
+''')
+    # disc=edu[15].split(',',1)
+txt3("**Programming**","`Python`, `JavaScript`, `Linux`,`Kotlin`, `XML`, `Java`, `Php`")
+txt3("**Data processing/wrangling**","`SQL, pandas, numpy`")
+txt3("**Data visualization**","`matplotlib`, `seaborn`")
+txt3("**Database**","`MySql`, `mongodb`")
+txt3("**Machine Learning**","`scikit-learn(**)`, `TensorFlow`")
+txt3("**Web development**","`Flask`,`Django`,`Reactjs`,`Nodejs`, `HTML`, `CSS`,`NodeJs`, `Php-Laravel`,`Java-Spring`, `ExpressJs`, `AngularJs`")
+txt3("**Model deployment**","`streamlit`, `Heroku`, `AWS`, `Digital Ocean`,`Google Cloud Computing`, `Microsoft Azure`, `AWS-Cloud`, `ngrok`, `Github`, `Pythonanywhere`")
+txt3("**App deployment**","`Android Studio`, `Flutter`, `Material Design`,`JavaScript`, `Kivy`, `reactjs`")
+txt3("**Other Tools**","`Python-Tkinter`, `Python-opencv`,`d3.js`")
+# 
+st.markdown('''
+## Profile Links
+''')
 
-if menu_id == "Past":
 
-# Print results.
-  for wasdo in Database:
-    with st.container():
-      if wasdo[10] != None:
-        txt3(wasdo[10],wasdo[11])
-        st.markdown("***")
-    
-    
-if menu_id == "Present":
+txt3("**Git Hub Link**","[Click Here](https://github.com/shivamkumarsingh28)")
+txt3("**LinkedIn Profile**","[Click Here](https://www.linkedin.com/in/shivam-singh-6a7212192/)")
+txt3("**SaeeAM Business Profile**","[Click Here](https://business.google.com/n/13598897696179258024/profile?hl=en-GB&fid=18438763562261313311)")
 
-# Print results.
-  for doing in Database:
-    if doing[8] != None:
-      txt3(doing[8],doing[9])
-      st.markdown("***")
-    
-if menu_id == "Future":
+#####################
 
-# Print results.
-  for willdo in Database:
-    if willdo[6] != None:
-      txt3(willdo[6], willdo[7])
-      st.markdown("***")
-    
-    
-if menu_id == "Achieve":
-  for achieve in Database:
-    with st.container():
-      if achieve[0] != None:
-        txt4(achieve[0], achieve[1], achieve[2])
-        st.markdown("***")
-    
+
+st.markdown('''
+## Project Links
+''')
+
+txt5("**Painter/Drawing/Sketch**", "- Created `Painter`.[Click Here]()", "https://youtu.be/0H2CIySbIX4")
+txt5("**Consultant Career Guider**", "- Created `Consultant`.[Click Here]()", "https://www.youtube.com/watch?v=3UwQ_dE8RzA")
+txt5("**Teaching/ Mentor**", "- Created `Teacher`.[Click Here]()", "https://www.youtube.com/watch?v=QxPMubY19Ak&t=8s")
+txt5("**Coding Portal**", "- Created `Coding Portal`.[Click Here]()", "https://youtu.be/bCbMtxfh97I")
+txt5("**Kitchen_Garden_Project#1**", "- Created `Kitchen Garden`.[Click Here]()", "https://youtu.be/8eDu0ffscrw")
+txt5("**Online_School_Project#2**", "- Created `Online School`.[Click Here]()", "https://www.youtube.com/watch?v=lB-rAgf1vI0")
+txt5("**WorkFromHome_Project#3**", "- Created `Workfromhome`.[Click Here]()", "https://youtu.be/I4rE_jWnXbU")
+txt5("**Online_Business_Project#4**", "- Created `Online Business`.[Click Here]()", "https://youtu.be/SAFWXAaJLTA")
+txt5("**Digital_Resume_Project#5", "- Created `Digital Resume`.[Click Here](https://github.com/shivamkumarsingh28/Shivam-Resume.git)", "https://www.youtube.com/watch?v=vCEE4VhrxeM")
+txt5("**Coding_Classes_Batch#3**", "- Created `Coding Classes`.[Click Here]() ", "https://www.youtube.com/watch?v=cLJMWbHLtIM")
+
